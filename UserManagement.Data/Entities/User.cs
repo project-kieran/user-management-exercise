737a757add12debc7dd04abf9c9b6a4471202b14
project-Kieran <mcdonough_kieran@yahoo.co.uk>
2023-11-06 11:58:33 +0000

    annotations in User.cs to prevent exceptions in context

--- UserManagement.Data/Entities/User.cs
+++ UserManagement.Data/Entities/User.cs
@@ -8,9 +8,12 @@ public class User
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
+    [Required(ErrorMessage = "Forename is required.")]
     public string Forename { get; set; } = default!;
+    [Required(ErrorMessage = "Surname is required.")]
     public string Surname { get; set; } = default!;
     public DateTime DateOfBirth { get; set; }
+    [Required(ErrorMessage = "Email is required.")]
     public string Email { get; set; } = default!;
     public bool IsActive { get; set; }
 }
